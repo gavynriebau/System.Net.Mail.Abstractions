0ec27333728c5b9f298bc542994ac1208d805792
Gavyn <gavyn.riebau@gmail.com>
2015-06-12 17:16:14 +0800

    Added more System.Net.Mail.SmtpClient properties

--- System.Net.Mail.Abstractions/ISmtpClient.cs
+++ System.Net.Mail.Abstractions/ISmtpClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,21 @@ namespace System.Net.Mail.Abstractions
     /// </summary>
     public interface ISmtpClient : IDisposable
     {
+        X509CertificateCollection ClientCertificates { get; }
+        ICredentialsByHost Credentials { get; set; }
+        SmtpDeliveryFormat DeliveryFormat { get; set; }
+        SmtpDeliveryMethod DeliveryMethod { get; set; }
+        bool EnableSsl { get; set; }
+        string Host { get; set; }
+        string PickupDirectoryLocation { get; set; }
+        int Port { get; set; }
+        ServicePoint ServicePoint { get; }
+        string TargetName { get; set; }
+        int Timeout { get; set; }
+        bool UseDefaultCredentials { get; set; }
+
+        event SendCompletedEventHandler SendCompleted;
+
         void Send(MailMessage message);
         void Send(string from, string recipients, string subject, string body);
         void SendAsync(MailMessage message, object userToken);
--- System.Net.Mail.Abstractions/SmtpClient.cs
+++ System.Net.Mail.Abstractions/SmtpClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,6 +21,154 @@ namespace System.Net.Mail.Abstractions
         public SmtpClient(Mail.SmtpClient smtpClient)
         {
             _smtpClient = smtpClient;
+        }
+
+        public X509CertificateCollection ClientCertificates
+        {
+            get
+            {
+                return _smtpClient.ClientCertificates;
+            }
+        }
+
+        public ICredentialsByHost Credentials
+        {
+            get
+            {
+                return _smtpClient.Credentials;
+            }
+            set
+            {
+                _smtpClient.Credentials = value;
+            }
+        }
+
+        public SmtpDeliveryFormat DeliveryFormat
+        {
+            get
+            {
+                return _smtpClient.DeliveryFormat;
+            }
+            set
+            {
+                _smtpClient.DeliveryFormat = value;
+            }
+        }
+
+        public SmtpDeliveryMethod DeliveryMethod
+        {
+            get
+            {
+                return _smtpClient.DeliveryMethod;
+            }
+            set
+            {
+                _smtpClient.DeliveryMethod = value;
+            }
+        }
+
+        public bool EnableSsl
+        {
+            get
+            {
+                return _smtpClient.EnableSsl;
+            }
+            set
+            {
+                _smtpClient.EnableSsl = value;
+            }
+        }
+
+        public string Host
+        {
+            get
+            {
+                return _smtpClient.Host;
+            }
+            set
+            {
+                _smtpClient.Host = value;
+            }
+        }
+
+        public string PickupDirectoryLocation
+        {
+            get
+            {
+                return _smtpClient.PickupDirectoryLocation;
+            }
+            set
+            {
+                _smtpClient.PickupDirectoryLocation = value;
+            }
+        }
+
+        public int Port
+        {
+            get
+            {
+                return _smtpClient.Port;
+            }
+            set
+            {
+                _smtpClient.Port = value;
+            }
+        }
+
+        public ServicePoint ServicePoint
+        {
+            get
+            {
+                return _smtpClient.ServicePoint;
+            }
+        }
+
+        public string TargetName
+        {
+            get
+            {
+                return _smtpClient.TargetName;
+            }
+            set
+            {
+                _smtpClient.TargetName = value;
+            }
+        }
+
+        public int Timeout
+        {
+            get
+            {
+                return _smtpClient.Timeout;
+            }
+            set
+            {
+                _smtpClient.Timeout = value;
+            }
+        }
+
+        public bool UseDefaultCredentials
+        {
+            get
+            {
+                return _smtpClient.UseDefaultCredentials;
+            }
+            set
+            {
+                _smtpClient.UseDefaultCredentials = value;
+            }
+        }
+
+        public event SendCompletedEventHandler SendCompleted
+        {
+            add
+            {
+                _smtpClient.SendCompleted += value;
+            }
+            remove
+            {
+                _smtpClient.SendCompleted -= value;
+            }
         }
 
         public void Send(MailMessage message)
